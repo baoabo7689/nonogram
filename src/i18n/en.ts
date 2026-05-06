6da5cd3329d8833cb9cfcd49e7c6a3c63c3136f9
Nguyen Trang Hong Bao <nthb@globeteam.com>
2026-05-06 14:32:57 +0700

    i

---
+++ src/i18n/en.ts
@@ -0,0 +1,20 @@
+export const translations = {
+  home: {
+    title: 'Welcome',
+    description: 'Welcome to the NextJS Template. Start building your app here.',
+  },
+  header: {
+    title: 'NextJS Template',
+    language: 'Language',
+    toggleLanguage: 'Change language',
+    contact: 'Contact',
+    help: 'Help',
+  },
+  help: {
+    title: 'Help',
+    common: 'Common',
+    commonHelps: ['Sample help for common section.'],
+  },
+};
+
+export default { translations };
